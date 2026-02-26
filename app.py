83716fac63295614984eccd07d9ed9030150ad98
Simarpreet Singh <owner@Owners-MacBook-Air.local>
2026-02-26 15:44:52 -0500

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # List of photos to display in the gallery
     photos = [
         "photos/us.jpeg",
         "photos/photo1.jpeg",
-        "photos/photo3.jpeg",
         "photos/photo2.jpeg",
+        "photos/photo3.jpeg"
     ]
+    
+    # Render the template with the photos only
     return render_template("index.html", photos=photos)
 
 if __name__ == "__main__":
